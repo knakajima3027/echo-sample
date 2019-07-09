b3e4cb5de5b1204ea2d697512f453ead928e0580
knakajima3027 <kamihate1012@gmail.com>
2019-07-09 15:09:37 +0900

    fix code

--- router/router.go
+++ router/router.go
@@ -7,10 +7,10 @@ import (
 
 func Router(e *echo.Echo) *echo.Echo {
 
-  e.GET("/hello", handler.HelloHandler)
-  e.GET("/:id", handler.IdHandler) // urlの値を読み取り表示する
+  e.GET("/hello",    handler.HelloHandler)
+  e.GET("/:id",      handler.IdHandler) // urlの値を読み取り表示する
   e.GET("/plus/:id", handler.IdPlusHandler)
-  e.GET("/json", handler.JsonHandler)
+  e.GET("/json",     handler.JsonHandler)
 
   return e
 }
